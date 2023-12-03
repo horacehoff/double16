f807c75dd58fbb5fda0a5740cbffecffa4aeb2a7
just_a_mango <horace.hoff@gmail.com>
2023-12-03 17:21:47 +0100

    /

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+
+function Home() {
+
+  return (
+    <>
+        <h1>
+            CODE. BUY. SELL
+        </h1>
+        <input type="text" placeholder="@email"/>
+        <br/><br/>
+        <button className="primary">CLICK ME</button>
+    </>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from './Home.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
 )
