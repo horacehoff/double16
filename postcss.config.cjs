de314ab49b13d0e263f1c8040f5c37668366331e
just_a_mango <horace.hoff@gmail.com>
2023-12-04 16:55:51 +0100

    /

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,13 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 
 module.exports = {
     plugins: [
         purgecss({
             content: ['./**/*.html', './**/*.jsx']
+        }),
+        cssnano({
+            preset: "default"
         })
     ]
 }
