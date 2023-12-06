e9ba7bc0625b98f428f616390178175f25fe497f
just_a_mango <horace.hoff@gmail.com>
2023-12-06 18:41:37 +0100

    /

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,19 @@
 import './Home.css'
+import Home1 from "./assets/home_1.png"
+import Home2 from "./assets/home_2.png"
 
 function Home() {
 
   return (
     <>
-        <h1>
-            CODE. BUY. SELL
-        </h1>
-        <input type="text" placeholder="@email"/>
-        <br/><br/>
-        <button className="primary">CLICK ME</button>
-        <br/><br/>
-        <button className="accent">CLICK ME ACCENT</button>
-        <br/><br/>
-        <a className="action-text">Action text</a>
-        <br/><br/>
-        <button className="action">PAGE_ACTION_BTN</button>
-        <br/><br/>
-        <select>
-            <option>
-                SELECT
-            </option>
-        </select>
+        <h1 className="buy-code-sell">CODE.<br/>BUY.<br/>SELL.</h1>
+        <img src={Home1} className="img1" alt="def fib(n)"/>
+        <img src={Home2} className="img2" alt="main() {}"/>
+        <p className="one-catchphrase">{"// 01 - CATCHPHRASE"}</p>
+        <p className="one-desc">Devshop allows anyone to buy or sell code snippets.<br/>
+            With DEVSHOP, you can easily share your code, and explore
+            the marketplace to find the code you need.</p>
+        {/*</ul>*/}
     </>
   )
 }
