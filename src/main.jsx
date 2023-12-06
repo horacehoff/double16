cc510a489155edc4c95fffd42aba9408a117f3d6
just_a_mango <horace.hoff@gmail.com>
2023-12-06 18:12:51 +0100

    /

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from './Home.jsx'
 import './index.css'
+import Navbar from "./Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <Navbar/>
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<Home/>}/>
