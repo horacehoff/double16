149e4c76100dc153ca663e61c7887d6b0fe8c684
just_a_mango <horace.hoff@gmail.com>
2023-12-23 12:53:37 +0100

    /

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -5,10 +5,13 @@ export default function SignUp() {
         <>
             <form className="sign-form">
                 <h1>SIGN_UP</h1>
-                <input type="text" placeholder="@username" name="username" required autoComplete="off"/><br/>
-                <input type="text" placeholder="@email" name="email" required autoComplete="off"/><br/>
-                <input type="text" placeholder="@password" name="psw" required autoComplete="off"/><br/>
-                <button type="submit" className="primary">SIGN UP</button>
+                <input type="text" placeholder="@username" name="username" autoComplete="username"/><br/>
+                <input type="email" placeholder="@email" name="email" autoComplete="email"/><br/>
+                <input type="password" placeholder="@password" name="psw" autoComplete="new-password"/><br/>
+                <button type="submit" className="primary" onClick={e => {
+                    e.preventDefault()
+                }}>SIGN UP
+                </button>
             </form>
         </>
     )
