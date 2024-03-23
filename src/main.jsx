ad7a6a7875301935bf1257900f7a89a8e2342446
just_a_mango <horace.hoff@gmail.com>
2024-03-23 12:50:36 +0100

    /

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import MostDownloaded from "./MostDownloaded.jsx";
 import Trending from "./Trending.jsx";
 import RecentSnippets from "./RecentSnippets.jsx";
 import About from "./About.jsx";
+import TestHome from "./testhome.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/home" element={<TestHome/>}/>
                 <Route path="/explore" element={<Explore/>}/>
                 <Route path="/search" element={<Search/>}/>
 
---
+++ src/testhome.jsx
@@ -0,0 +1,145 @@
+import "./testhome.css"
+import logo from "./assets/navlogo.svg"
+import {useEffect} from "react";
+
+export default function TestHome() {
+
+    const wrapper = <>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+        <img src={logo}/>
+    </>
+
+    const wrappertwo = <>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+        <p>CODE.BUY.SELL.</p>
+    </>
+
+    useEffect(() => {
+        // document.querySelector(".nav").style.display = "none"
+        document.querySelector(".nav").style.opacity = 0
+        document.querySelector(".nav").style.backgroundColor = "black"
+        document.getElementById("home-gif").style.opacity = 0
+        setTimeout(() => {
+            document.querySelector(".nav").style.opacity = 1
+            document.getElementById("home-gif").style.opacity = 1
+        }, 1000)
+
+
+        function randomFloat(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        function infiniteHorizontalScrolling() {
+            document.querySelectorAll(".home-wrapper-bg-slide").forEach((elem) => {
+                const scrollSpeed = randomFloat(0.1, 0.5); // Adjust scrolling speed as needed
+                const contentWidth = elem.scrollWidth;
+
+                // Clone the contents to the right side
+                const clonedContents = elem.innerHTML;
+                elem.innerHTML += clonedContents;
+
+                let scrollPosition = 0;
+
+                function scroll() {
+                    scrollPosition += scrollSpeed;
+                    if (scrollPosition >= contentWidth) {
+                        scrollPosition = 0;
+                    }
+                    elem.scrollLeft = scrollPosition;
+                    requestAnimationFrame(scroll);
+                }
+
+                scroll();
+            })
+        }
+
+        infiniteHorizontalScrolling();
+
+
+        function mapRange(value, fromMin, fromMax, toMin, toMax) {
+            // Scale the value from the original range to the new range
+            return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
+        }
+
+
+        addEventListener("scroll", e => {
+            let image_size = mapRange(document.documentElement.scrollTop, 0, 433, 500, 1000)
+            let image_brightness = mapRange(document.documentElement.scrollTop, 0, 433, 1, 0)
+            let image_blur = mapRange(document.documentElement.scrollTop, 0, 433, 0, 10)
+            document.getElementById("home-gif").style.width = image_size + "px"
+            document.getElementById("home-gif").style.opacity = image_brightness
+            document.getElementById("home-gif").style.filter = "blur(" + image_blur + "px)"
+            document.getElementById("home-wrapper").style.opacity = image_brightness
+            console.log(document.documentElement.scrollTop)
+
+            let subtitle_size = 0
+            if (matchMedia("(max-width: 600px)").matches) {
+                subtitle_size = mapRange(document.documentElement.scrollTop, 300, 500, 1, 5)
+            } else {
+                subtitle_size = mapRange(document.documentElement.scrollTop, 300, 500, 1, 2.6)
+            }
+            if (subtitle_size <= 2.6) {
+                document.getElementById("home-content-subtitle").style.fontSize = subtitle_size + "vw"
+            }
+        })
+
+    }, [])
+    return (
+        <>
+            <div className="home-wrapper" id="home-wrapper">
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrappertwo}</div>
+                <div className="home-wrapper-bg-slide" id="scrollingDiv">{wrapper}</div>
+            </div>
+
+
+            <div className="home-content">
+                <img src={logo}/>
+                <h1 id="home-content-subtitle">CODE MARKETPLACE MADE FOR BUYING AND SELLING CODE SNIPPETS</h1>
+                <br/><br/>
+                <div className="home-content-action">
+                    <button className="action">🔎 EXPLORE THE MARKETPLACE</button>
+                    <button className="action">🔗 SHARE</button>
+                </div>
+            </div>
+        </>
+    )
+}
